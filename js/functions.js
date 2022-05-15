4773b010335c369a9cdcfe79cfdc4ae919d7728b
Turtle <turtle84375@programmer.net>
2022-05-14 20:57:09 -1000

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -25,10 +25,14 @@ function getCookie(name) {
     return "";
 }
 
-function validate() {
-  var token = getCookie(auth);
-  if (token == "") {
-    window.location.href = "/";
-  } else {
-    window.location.href = "dashboard";
-};
+function authorize() {
+  var cookieArr = document.cookie.split(";");
+  for(var i=0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split("=");
+    if("auth" == cookiePair[0].trim()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
