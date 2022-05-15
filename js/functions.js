524718e0a8076f07b669126da3677a543ea6bc37
Turtle <turtle84375@programmer.net>
2022-05-14 20:58:30 -1000

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,3 @@
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-};
-
-function getCookie(name) {
-    // Split cookie string and get all individual name=value pairs in an array
-    var cookieArr = document.cookie.split(";");
-    
-    // Loop through the array elements
-    for(var i = 0; i < cookieArr.length; i++) {
-        var cookiePair = cookieArr[i].split("=");
-        
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
-        if(name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    
-    // Return null if not found
-    return "";
-}
-
 function authorize() {
   var cookieArr = document.cookie.split(";");
   for(var i=0; i < cookieArr.length; i++) {
